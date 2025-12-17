5026069de66e84e30f024120e43d324afff6046c
Gryisic <87720553+Gryisic@users.noreply.github.com>
2025-12-17 17:23:11 +0500

    Refactor

---
+++ foxypack_x_twikit/answers.py
@@ -0,0 +1,45 @@
+from enum import Enum
+import uuid
+from pydantic import Field
+from foxypack.foxypack_abc.answers import (
+    AnswersAnalysis,
+    AnswersSocialContent,
+    AnswersSocialContainer,
+)
+
+
+class TwitterEnum(Enum):
+    tweet = "tweet"
+    profile = "profile"
+
+
+class TwitterAnswersAnalysis(AnswersAnalysis):
+    answer_id: str = Field(default_factory=lambda: uuid.uuid4().hex)
+    code: str
+
+
+class TwitterTweetAnswersStatistics(AnswersSocialContent):
+    tweet_id: str
+    user_id: str
+    username: str
+    text: str
+    like_count: int
+    retweet_count: int
+    reply_count: int
+    quote_count: int
+    bookmark_count: int
+    language: str
+
+
+class TwitterProfileAnswersStatistics(AnswersSocialContainer):
+    user_id: str
+    username: str
+    screen_name: str
+    following_count: int
+    tweet_count: int
+    listed_count: int
+    favourites_count: int
+    verified: bool
+    description: str
+    location: str
+    profile_image_url: str
